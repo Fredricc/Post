e86d42a49a7c5d37c6b6237a0b9e8431e8469be0
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-15 03:25:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Post
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post
+            {
+                Id = 001,
+                Title = "Test Title",
+                Description = "Test",
+                PublishingDate = DateTime.Now,
+                
+            };
+
+            Console.WriteLine(post.Id);
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Description);
+            Console.WriteLine(post.PublishingDate);
+        }
+    }
+}
---
+++ post.cs
@@ -0,0 +1,18 @@
+﻿namespace Post
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime PublishingDate { get; set; }
+        public enum Rating
+        
+        {
+            Bad = 1,
+            Good = 2,
+            VeryGood = 3,
+            Excellent = 4
+        }
+    }
+}
